14af0dbb7353ffc6f1591fa00f5ee893dc1dfa96
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2025-07-09 17:33:24 +0000

    fix ssh tunnel enable/disable

--- tofu/modules/generic/test_environment/main.tf
+++ tofu/modules/generic/test_environment/main.tf
@@ -26,6 +26,7 @@ module "upstream_cluster" {
   reserve_node_for_monitoring = var.upstream_cluster.reserve_node_for_monitoring
   enable_audit_log            = var.upstream_cluster.enable_audit_log
   create_tunnels              = var.upstream_cluster.create_tunnels
+  public                      = var.upstream_cluster.public_ip
 
   sans                      = ["upstream.local.gd"]
   local_kubernetes_api_port = var.first_kubernetes_api_port
@@ -49,6 +50,7 @@ module "tester_cluster" {
   reserve_node_for_monitoring = var.tester_cluster.reserve_node_for_monitoring
   enable_audit_log            = var.tester_cluster.enable_audit_log
   create_tunnels              = var.tester_cluster.create_tunnels
+  public                      = var.tester_cluster.public_ip
 
   sans                      = ["tester.local.gd"]
   local_kubernetes_api_port = var.first_kubernetes_api_port + 1
@@ -72,6 +74,8 @@ module "downstream_clusters" {
   reserve_node_for_monitoring = local.downstream_clusters[count.index].reserve_node_for_monitoring
   enable_audit_log            = local.downstream_clusters[count.index].enable_audit_log
   create_tunnels              = local.downstream_clusters[count.index].create_tunnels
+  public                      = local.downstream_clusters[count.index].public_ip
+
   sans                        = ["${local.downstream_clusters[count.index].name}.local.gd"]
   local_kubernetes_api_port   = var.first_kubernetes_api_port + 2 + count.index
   tunnel_app_http_port        = var.first_app_http_port + 2 + count.index
@@ -84,7 +88,6 @@ module "downstream_clusters" {
 }
 
 module "nodes" {
-  # for_each = {for node in local.nodes: node.name => node}
   count = length(local.nodes)
   source = "../node"
   project_name          = var.project_name
